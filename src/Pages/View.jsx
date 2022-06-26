e2200881ed303a487ec2c9f0c33ed23e26440a23
ACuteWoof <sutharshanvithushan@gmail.com>
2022-06-26 10:25:07 +0530

    remove unused var

--- src/Pages/View.jsx
+++ src/Pages/View.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
