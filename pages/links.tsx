46353ef6203e3dbff65b49a945a56526a5f000c8
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-05-20 23:06:26 +0000

    Links

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Links() {
-    return( <div className="flex flex-row gap-4 items-center justify-center ">
-        <a href="/" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Home</a>
-        <a href="https://acutewoof.is-a.dev" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Portfolio</a>
-        <a href="https://buymeacoffee.com/acutewoof" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Donate</a>
+    return (<div className="flex flex-row gap-4 items-center justify-center ">
+        <Link href="/" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Home</Link>
+        <Link href="https://acutewoof.is-a.dev" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Portfolio</Link>
+        <Link href="https://buymeacoffee.com/acutewoof" className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-300">Donate</Link>
+
     </div>
     )
 }
